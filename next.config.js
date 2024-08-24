382630f5931634fe20e3a1f8a156588aa0462e4b
Edward Shin <pobredward@gmail.com>
2024-08-24 05:10:31 +0000

    Improve Performance

--- next.config.js
+++ next.config.js
@@ -7,6 +7,37 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimize = true;
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+            output: {
+              comments: false,
+            },
+          },
+        }),
+      ];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
