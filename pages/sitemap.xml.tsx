2bf187219c77a464a656650fcf624bf7373cfaf8
Edward Shin <pobredward@gmail.com>
2024-08-28 01:50:14 +0000

    Update "SEO"

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next";
 import { db } from "../lib/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const Sitemap = () => {
   return null;
@@ -9,22 +9,79 @@ const Sitemap = () => {
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const baseUrl = "https://inschoolz.com";
 
-  const postsSnapshot = await getDocs(collection(db, "posts"));
+  const postsSnapshot = await getDocs(
+    query(
+      collection(db, "posts"),
+      where("categoryId", "in", [
+        "national-free",
+        "national-hot",
+        "national-minor-develop",
+        "national-minor-humor",
+        "national-minor-esports",
+        "national-minor-webtoon",
+        "national-minor-movie",
+        "national-minor-kpop",
+        "national-minor-love",
+        "national-minor-sports",
+        "national-minor-trip",
+        "national-minor-food",
+        "national-minor-animal",
+        "national-minor-study",
+        "national-minor-career",
+        "national-minor-language",
+        "national-minor-coding",
+      ])
+    )
+  );
+
   const posts = postsSnapshot.docs.map((doc) => ({
     id: doc.id,
     categoryId: doc.data().categoryId,
+    title: doc.data().title,
+    content: doc.data().content,
+    createdAt: doc.data().createdAt.toDate().toISOString(),
   }));
 
   const staticPages = ["", "game", "ranking", "community"].map(
-    (staticPagePath) => `${baseUrl}/${staticPagePath}`,
+    (staticPagePath) => `${baseUrl}/${staticPagePath}`
   );
 
-  const dynamicPages = posts.map(
-    (post) => `${baseUrl}/community/${post.categoryId}/${post.id}`,
-  );
+  const getCategoryName = (categoryId) => {
+    const categoryMap = {
+      "national-free": "자유 게시판",
+      "national-hot": "HOT 게시글",
+      "national-minor-develop": "제안사항",
+      "national-minor-humor": "유머/밈",
+      "national-minor-esports": "모바일/PC게임",
+      "national-minor-webtoon": "웹툰/만화",
+      "national-minor-movie": "영화/드라마",
+      "national-minor-kpop": "아이돌/KPOP",
+      "national-minor-love": "연애",
+      "national-minor-sports": "운동",
+      "national-minor-trip": "여행",
+      "national-minor-food": "맛집/요리",
+      "national-minor-animal": "반려동물/동물",
+      "national-minor-study": "공부/입시",
+      "national-minor-career": "진로/취업",
+      "national-minor-language": "영어/외국어",
+      "national-minor-coding": "코딩/IT",
+    };
+    return categoryMap[categoryId] || "기타 게시판";
+  };
+
+  const dynamicPages = posts.map((post) => ({
+    url: `${baseUrl}/community/${post.categoryId}/${post.id}`,
+    lastmod: post.createdAt,
+    title: `${post.title} - ${getCategoryName(post.categoryId)}`,
+    content: post.content.substring(0, 200),
+  }));
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+            xmlns:xhtml="http://www.w3.org/1999/xhtml"
+            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+            xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
       ${staticPages
         .map((url) => {
           return `
@@ -37,12 +94,23 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
         })
         .join("")}
       ${dynamicPages
-        .map((url) => {
+        .map(({ url, lastmod, title, content }) => {
           return `
             <url>
               <loc>${url}</loc>
+              <lastmod>${lastmod}</lastmod>
               <changefreq>weekly</changefreq>
               <priority>0.9</priority>
+              <news:news>
+                <news:publication>
+                  <news:name>인스쿨즈</news:name>
+                  <news:language>ko</news:language>
+                </news:publication>
+                <news:publication_date>${lastmod}</news:publication_date>
+                <news:title>${title}</news:title>
+              </news:news>
+              <xhtml:link rel="alternate" hreflang="ko" href="${url}"/>
+              <xhtml:link rel="alternate" hreflang="x-default" href="${url}"/>
             </url>
           `;
         })
